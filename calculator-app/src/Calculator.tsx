c9fc077bdd6eb1964db1325c8c62f5af91616bf8
RoseMagura <magurarm@guilford.edu>
2021-05-08 22:19:22 -0400

    Feat: Add input and clear number functionality

--- calculator-app/src/Calculator.tsx
+++ calculator-app/src/Calculator.tsx
@@ -3,26 +3,47 @@ import { Button } from '@material-ui/core';
 
 export const Calculator = () => {
     const [currVal, setVal] = useState(0);
+
+    const inputNum = (val: number) => {
+        currVal === 0
+            ? setVal(val)
+            : !(val === 0 && currVal === 0) && setVal(parseInt(currVal + String(val)));
+    }
+
+    const clearDisplay = () => {
+        setVal(0);
+    }
+
+    const addDecimal = () => {
+        console.log('adding decimal');
+    }
+
     const createGrid = () => {
-        const items: any[] = [];
+        const numNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        const items: JSX.Element[] = [];
         for (let i = 0; i < 10; i++) {
-            items.push(i);
+            items.push(
+                <Button id={numNames[i]}
+                    onClick={event => inputNum(i)} key={i}>
+                    {i}
+                </Button>
+            );
         }
         return items;
     }
+
     const numGrid = createGrid();
     return (
         <div>
             <h2 id='display'>{currVal}</h2>
-            <Button>AC</Button>
-            <Button>/</Button>
-            <Button>X</Button>
-            {/* number grid here */}
+            <Button id='clear' onClick={clearDisplay}>AC</Button>
+            <Button id='divide'>/</Button>
+            <Button id='multiply'>X</Button>
             {numGrid}
-            <Button>.</Button>
-            <Button>-</Button>
-            <Button>+</Button>
-            <Button>=</Button>
+            <Button id='decimal' onClick={addDecimal}>.</Button>
+            <Button id='subtract'>-</Button>
+            <Button id='add'>+</Button>
+            <Button id='equals'>=</Button>
         </div>
     )
 }
