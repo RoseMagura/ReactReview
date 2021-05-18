10f9c17f0040302ab1346bed8f8cba488d91f04b
RoseMagura <magurarm@guilford.edu>
2021-05-18 16:27:20 -0400

    Feat: Working on additional timer functionality

--- pomodoro/src/containers/Active.tsx
+++ pomodoro/src/containers/Active.tsx
@@ -15,24 +15,55 @@ const Active = () => {
     const [displayString, setDisplay] = useState('');
     const [running, toggleRunning] = useState(false);
     const [timerId, setTimerId] = useState<NodeJS.Timeout>();
+    const [currLength, setCurrLength] = useState(0);
 
-    const [currLength, setCurrLength] = useState(5); // TODO: Update this later
     const dispatch = useDispatch();
 
     useEffect(() => {
-        activeType === 'Session'
-            ? setDisplay(`${sessionLength}:00`)
-            : setDisplay(`${breakLength}:00`);
-    }, [activeType]);
+        if (activeType === 'Session') {
+            setCurrLength(sessionLength * 60); // in seconds
+            // setDisplay(`${sessionLength}:00`);
+        } else {
+            setCurrLength(breakLength * 60);
+            // setDisplay(`${breakLength}:00`)
+        }
+    }, [activeType, sessionLength, breakLength]);
+
+    useEffect(() => {
+        setDisplay(`${currLength}`);
+    }, [currLength]);
+
+    const reset = () => {
+        console.log('resetting');
+        if (activeType === 'Session') {
+            setCurrLength(sessionLength * 60); // in seconds
+            // setDisplay(`${sessionLength}:00`);
+        } else {
+            setCurrLength(breakLength * 60);
+            // setDisplay(`${breakLength}:00`)
+        }
+        toggleRunning(!running);
+        timerId !== undefined && clearInterval(timerId);
+    }
 
     const toggle = () => {
+        reset();
         activeType === 'Session'
             ? dispatch(toggleActive('Break'))
             : dispatch(toggleActive('Session'));
     }
 
-    const reset = () => {
-        console.log('resetting');
+    const convertSeconds = (seconds: number) => {
+        const minutes = seconds / 60;
+        const remainingSec = seconds % 60;
+        console.log(minutes, remainingSec);
+        let formattedString = `${minutes}:`;
+        if (remainingSec < 10) {
+            formattedString += `0${remainingSec}`;
+        } else {
+            formattedString += `${remainingSec}`;
+        }
+        return formattedString;
     }
 
     const startStop = () => {
@@ -44,7 +75,6 @@ const Active = () => {
             let count = 1;
             const timer = setInterval(() => {
                 let localLen = currLength - count;
-                // setDisplay(`${currLength}`);
                 setCurrLength(localLen);
                 count++;
             }, 1000);
@@ -55,7 +85,7 @@ const Active = () => {
 
     // Stop automatically when session or break reaches 0
     // TODO: Make sure 0:00
-    if(currLength === 0){
+    if (currLength === 0) {
         timerId !== undefined && clearInterval(timerId);
         // TODO: Play Sound here 
         // TODO: Start new break or session accordingly
@@ -64,11 +94,12 @@ const Active = () => {
     return (
         <div>
             <h2>{activeType}</h2>
+            <h2>{displayString}</h2>
             <h2>{currLength}</h2>
             <button id='start_stop' onClick={startStop}>Play / Pause</button>
-            <div>{running && 'Running'}</div>
             <button id='reset' onClick={reset}>Reset</button>
             <button onClick={toggle}>Toggle Session / Break</button>
+            <div>{running && 'Running'}</div>
         </div>
     )
 }
